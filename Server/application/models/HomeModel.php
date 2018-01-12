a027adc93ce54cf310c48aa00c5735962c286bdb
Kavindu <kavinduxyz@gmail.com>
2018-01-12 15:53:15 +0530

    modify homemodel

--- Server/application/models/HomeModel.php
+++ Server/application/models/HomeModel.php
@@ -1,8 +1,20 @@
 <?php 
 
 class HomeModel extends CI_Model{
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+	}
 	public function getData(){
-		$query = $this->db->get('user');
+		$query = $this->db->get('thesidevelopment');
+		return $query->result();
+	}
+
+	public function get_all_users()
+	{
+		$query=$this->db->get('users');
 		return $query->result();
 	}
 }
