76e9f24c2a30072ecac12c20b655e51b3a5cdfdc
Kavindu <kavinduxyz@gmail.com>
2018-01-12 14:38:26 +0530

    POC for display view using constuct

--- Server/application/controllers/homeController.php
+++ Server/application/controllers/homeController.php
@@ -1,12 +1,23 @@
 <?php
+if(!defined('BASEPATH')) exit('No direct script access allowed');
+class homeController extends CI_Controller {
+	// declare variables or class properties
+	var $name;
+	var $color;
 
-class HomeController extends CI_Controller {
-	public function index(){
-		$this->load->model('HomeModel');
-		$data['records'] = $this->HomeModel->getData();
+	function __construct(){
+		parent::__construct();
+		// give default value
+		$this->name="ABC";
+		$this->color="red";
+	}
+
+	//http://localhost:8012/thesi/Server/index.php/HomeController/you
+	public function you(){
+		$data['name']=$this->name;
+		$data['color']=$this->color;
 
 		$this->load->view('HomeView', $data);
 	}
 }
-
 ?>
