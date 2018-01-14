7dd27f98b09d928d1c996aca489dde9e60d6a18d
Ishanka Madhuranga <ishanka@hbs.com>
2018-01-14 23:22:20 +0530

    Modify the base url

--- Server/application/config/config.php
+++ Server/application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8012/ppp/Server/';
+$config['base_url'] = 'http://localhost:8012/Thesi/Server/';
 
 /*
 |--------------------------------------------------------------------------
